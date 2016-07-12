a219420b28fd483ee669cfba8120a727676f25c2
Eric Shi <eshi@ucsc.edu>
2016-07-11 20:53:18 -0700

    Fixing git?

---
+++ slugspot/app/src/main/java/com/example/erics/slugspot/about.java
@@ -0,0 +1,20 @@
+package com.example.erics.slugspot;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+/**
+ * Created by EricS on 7/8/2016.
+ */
+public class about extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
