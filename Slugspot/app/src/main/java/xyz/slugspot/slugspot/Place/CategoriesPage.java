8d800f248aea6b835c16b9fcbcb3256c80c2c344
Jared Jensen <j@redj.xyz>
2016-07-22 16:31:03 -0700

    Updated logo, added categories page

---
+++ Slugspot/app/src/main/java/xyz/slugspot/slugspot/Place/CategoriesPage.java
@@ -0,0 +1,13 @@
+package xyz.slugspot.slugspot.Place;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoriesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories_page);
+    }
+}
