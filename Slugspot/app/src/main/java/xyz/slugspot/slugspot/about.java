1a7984872ebf2087a29583ad56bbe5f59520569a
Jared Jensen <j@redj.xyz>
2016-07-25 13:56:35 -0700

    Added about page

--- Slugspot/app/src/main/java/xyz/slugspot/slugspot/about.java
+++ Slugspot/app/src/main/java/xyz/slugspot/slugspot/about.java
@@ -9,11 +9,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
 public class about extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+        ImageView image = (ImageView) findViewById(R.id.imageView1);
+        image.setImageResource(R.drawable.logo);
+
     }
 }
